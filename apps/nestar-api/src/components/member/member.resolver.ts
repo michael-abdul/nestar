8d9afc9b6438b16714fc29405c52d9680430976d
michael michaeal <michael@michaels-MacBook-Pro.local>
2024-05-17 11:09:11 +0900

    feat: develop getAgents GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { MemberInput, LoginInput } from '../../libs/dto/member/member.input';
-import { Member } from '../../libs/dto/member/member';
+import { MemberInput, LoginInput, AgentsInquiry } from '../../libs/dto/member/member.input';
+import { Member, Members } from '../../libs/dto/member/member';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
@@ -65,7 +65,14 @@ export class MemberResolver {
 		const targetId = shapeIntoMongoObjectId(input);
 		return this.memberService.getMember(memberId, targetId);
 	}
+	 @UseGuards(WithoutGuard)
+	 @Query(()=> Members)
+	public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
+		console.log('Query: getAgents');
+		return this.memberService.getAgents(memberId, input);
 
+	}
+	 
 	/**ADMIN **/
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
