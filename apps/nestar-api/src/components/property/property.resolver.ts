834b616c0e8a4c165ea14cd6bf69bb4b7d266f56
michael michaeal <michael@michaels-MacBook-Pro.local>
2024-05-20 09:45:22 +0900

    fix: modification of property module

---
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver } from '@nestjs/graphql';
+import { PropertyService } from './property.service';
+
+@Resolver()
+export class PropertyResolver {
+constructor(private readonly propertyService: PropertyService) {}
+
+}
